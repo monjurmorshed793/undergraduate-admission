adfe7ef2cdaac5c7850f1f8692f632ce53950d03
monju <monjurmorshed793@gmail.com>
2020-03-06 15:56:04 +0600

    Configuration icon changed in extended navbar.

--- src/main/webapp/app/core/icons/font-awesome-icons.ts
+++ src/main/webapp/app/core/icons/font-awesome-icons.ts
@@ -32,7 +32,8 @@ import {
   faTrashAlt,
   faAsterisk,
   faTasks,
-  faHome
+  faHome,
+  faCog
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -70,6 +71,7 @@ export const fontAwesomeIcons = [
   faCalendarAlt,
   faSearch,
   faTrashAlt,
-  faAsterisk
+  faAsterisk,
+  faCog
   // jhipster-needle-add-icon-import
 ];
